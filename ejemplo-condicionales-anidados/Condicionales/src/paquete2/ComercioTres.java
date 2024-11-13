004fbf4acb69bebb46ba8c70961093b061f556fa
Emilio J Cueva <ecueva611@gmail.com>
2024-11-13 11:58:12 -0500

    Primer Cambio

--- ejemplo-condicionales-anidados/Condicionales/src/paquete2/ComercioTres.java
+++ ejemplo-condicionales-anidados/Condicionales/src/paquete2/ComercioTres.java
@@ -1,6 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Existe una modificación en el proceso de la problemática el porcentaje del 
+descuento por seguro será ingresado por teclado. Considerar los valores posibles
+a ingresar son entre uno y quince si la persona ingresa un valor fuera de este 
+rango el valor del porcentaje va a ser 10
  */
 package paquete2;
 
@@ -11,72 +15,82 @@ import java.util.Scanner;
  *
  * @author reroes
  */
-public class Comercio {
-    
+public class ComercioTres {
+
     public static void main(String[] args) {
-        
+
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US); // 10,2 / 10.2
-        
+
         /*Hay que analizar el problema hasta el reporte para entender las 
         variables fijas*/
-        
         String nombre;
         String apellido;
         int edad;
         double sueldoBasico;
         int numeroProductos;
-        
+
         double productividad;
         double coeficiente = 0.6;
-        double bono  = 0;
-        
-        double porcentajeSeguro = 8;
+        double bono = 0;
+
+        double porcentajeSeguro;
+        double porcentajeFinal = 0;
         double adicionalSeguro;
-        
-        double sueldoFinal; 
-        
+
+        double sueldoFinal;
+
         System.out.println("Ingrese el nombre por favor");
         nombre = entrada.nextLine();
-        
+
         System.out.println("Ingrese el apellido por favor");
         apellido = entrada.nextLine();
-        
+
         System.out.println("Ingrese la edad por favor");
         edad = entrada.nextInt();
 
         System.out.println("Ingrese el sueldo básico por favor");
-        sueldoBasico = entrada.nextDouble();        
-        
+        sueldoBasico = entrada.nextDouble();
+
+        System.out.println("Ingrese el porcentaje del seguro");
+        porcentajeSeguro = entrada.nextDouble();
+
         System.out.println("Ingrese el número de productos vendidos "
                 + "por favor");
         numeroProductos = entrada.nextInt();
-        
+
         // calcular la productividad
         productividad = numeroProductos * coeficiente;
-        
-        if (productividad<=30){
+
+        if (productividad <= 30) {
             bono = 25; // $25
-        }else{
-            if (productividad>=31 && productividad<80){
+        } else {
+            if (productividad >= 31 && productividad < 80) {
                 bono = 50;
-            }else{
-                if (productividad>=80 && productividad < 200) {
+            } else {
+                if (productividad >= 80 && productividad < 200) {
                     bono = 100;
-                }else{
-                    if(productividad >= 200){
+                } else {
+                    if (productividad >= 200) {
                         bono = 200;
                     }
                 }
             }
         }
-        
-       
-        adicionalSeguro = (sueldoBasico * porcentajeSeguro)/100; /*es mejor
+        if (porcentajeSeguro >= 1 && porcentajeSeguro <= 15) {
+            porcentajeFinal = porcentajeSeguro;
+        } else {
+            if (porcentajeSeguro < 1 && porcentajeSeguro > 15) {
+                porcentajeFinal = 10;
+            }
+        }
+
+        adicionalSeguro = (sueldoBasico * porcentajeFinal) / 100;
+        /*es mejor
         usar una variable que un numero
-        */
+         */
         sueldoFinal = adicionalSeguro + bono + sueldoBasico;
-        
+
         System.out.printf("Reporte de Empleado EL GRAN COMERCIO\n\n"
                 + "Nombres: %s\n"
                 + "Apellidos: %s\n"
@@ -86,8 +100,8 @@ public class Comercio {
                 + "\tProductidad: %.2f\n"
                 + "\tBono: $%.2f\n"
                 + "Adicional del seguro: $%.2f\n\n"
-                + "Sueldo final: $%.2f\n", 
-                nombre, 
+                + "Sueldo final: $%.2f\n",
+                nombre,
                 apellido,
                 edad,
                 sueldoBasico,
@@ -96,8 +110,8 @@ public class Comercio {
                 bono,
                 adicionalSeguro,
                 sueldoFinal);
-        
+
         //
     }
-    
+
 }
